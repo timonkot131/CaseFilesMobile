2536a0f39adad2bf147e185ec7b9b29bd88f962b
timonkot131 <timon01@list.ru>
2020-10-01 15:48:49 +0400

    Initial commit

---
+++ app/src/main/java/com/example/casefilesmobile/AuthActivity.kt
@@ -0,0 +1,15 @@
+package com.example.casefilesmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+
+
+
+    }
+}
